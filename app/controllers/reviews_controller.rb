d254d43888a2bacc5c789e0288a4972a55ae45e4
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-02-20 16:03:45 +0900

    13

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,15 @@
 class ReviewsController < ApplicationController
 
   def index
-
     @reviews = Review.all
     @reviews.each do |review|
       if current_user.id == review.background_id
         @review = Review.find_by(background_id: review.background_id)
+        @only_true_review = @review.attributes.select{|k, v| v ==  true} # @reviewからattributesで属性をハッシュ形式で取得後にtrueの値のみをインスタンス変数に代入
       end
     end
-
-      set_target_columns
-      key = set_target_columns.map {|c| c.to_sym}
-      value = set_target_columns
-
-    if key.count == value.count
-      hash = key.zip(value)
-    end
-
+    set_target_columns
   end
-
 
   def new
     @review = Review.new
@@ -32,25 +23,24 @@ class ReviewsController < ApplicationController
     redirect_to reviews_path
   end
 
-    private
+  private
 
-      def set_target_columns # id,background_id, created_at, updated_at を取り除いたReviweモデルのカラム名の配列を作成
-        columns = Review.column_names
-        @target_columns = columns[2...-2]
-      end
+    def set_target_columns # Reviweモデルのカラム名からid,background_id, created_at, updated_at カラムを取り除いた配列を作成するメソッド
+      columns = Review.column_names
+      @target_columns = columns[2...-2] # HACK: Reviewsテーブルにカラム追加した際に代入する要素がずれる可能性がある
+    end
 
-      def set_review
-        @reviews = Review.all
-        @reviews.each do |review|
-          if current_user.id == review.background_id
-            @review = Review.find_by(background_id: review.background_id)
-          end
+    def set_review
+      @reviews = Review.all
+      @reviews.each do |review|
+        if current_user.id == review.background_id
+          @review = Review.find_by(background_id: review.background_id)
         end
       end
+    end
 
-      def reviews_params
-        params.require(:review).permit(Review.column_names.map{|c| c.to_sym})
-      end
-
+    def reviews_params
+      params.require(:review).permit(Review.column_names.map{|c| c.to_sym})
+    end
 
 end
